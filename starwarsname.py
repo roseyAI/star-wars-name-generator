5d70e2f9bd149721ef16afc9a98d0ea7697e1731
Rose R <65215936+roseyAI@users.noreply.github.com>
2024-05-27 06:56:13 -0700

    added key identifier to remove duplicate error

--- starwarsname.py
+++ starwarsname.py
@@ -8,11 +8,10 @@ Find out your Star Wars name today in just a few clicks!
 st.markdown("![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeXUyajk4NHk0YXc0Zzl6cHFqcXQ1b2lraW42ZjlmM3JjZTZmbWlhdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3ohuPdEqZR8tDeuN3O/giphy.gif)")
 
 # Use Streamlit's text input to get user inputs
-firstName = st.text_input("Enter your first name:").title()
-lastName = st.text_input("Enter your last name:").lower()
-maidenName = st.text_input("Enter your mother's maiden name:").title()
-cityName = st.text_input("Enter the city you were born:").lower()
-
+firstName = st.text_input("Enter your first name:", key="first_name").title()
+lastName = st.text_input("Enter your last name:", key="last_name").lower()
+maidenName = st.text_input("Enter your mother's maiden name:", key="maiden_name").title()
+cityName = st.text_input("Enter the city you were born:", key="city_name").lower()
 
 # Add a button to generate the Star Wars name
 if st.button('Generate'):
@@ -30,7 +29,7 @@ if st.button('Generate'):
 
 # Add a button to clear the fields
 if st.button("Clear Fields"):
-    firstName = st.text_input("Enter your first name:").title()
-    lastName = st.text_input("Enter your last name:").lower()
-    maidenName = st.text_input("Enter your mother's maiden name:").title()
-    cityName = st.text_input("Enter the city you were born:").lower()
+    firstName = ""
+    lastName = ""
+    maidenName = ""
+    cityName = ""
